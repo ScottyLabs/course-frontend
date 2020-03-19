3cfa767eb94a20e81865dda8c833819d5c4a3df9
David Ethan Hwang <hdavidethan@gmail.com>
2020-03-19 16:36:57 -0400

    Init React App w/ Bootstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <h1>Hello, World!</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
