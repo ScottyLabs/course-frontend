cc36493a5a4fc0ec789aa069aa36cbd8bb6112cc
David Ethan Hwang <hdavidethan@gmail.com>
2020-04-11 14:59:06 -0400

    Added production code

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 3002;
+const app = express();
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port, function () {
+    return console.log('App listening on port ' + port + '.');
+});
