628d7e1eb3260976f2d27c30a9e5b26f12d155f9
David Ethan Hwang <hdavidethan@gmail.com>
2020-10-16 04:43:36 -0400

    Google analytics

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import { Main } from "./pages/Main";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
+  // Google Analytics
+  if (process.env.REACT_APP_ENV === "production") {
+    const script1 = document.createElement("script");
+    script1.async = true;
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=UA-180645492-1";
+    const script2 = document.createElement("script");
+    script2.innerHTML =
+      "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-180645492-1');";
+
+    document.head.append(script1);
+    document.head.append(script2);
+  }
+
   return (
     <Router>
       <Route path="/" component={Main} />
