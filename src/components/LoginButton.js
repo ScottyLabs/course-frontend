60ddc47f1c90696da59007ca235c4329276d2899
Bradley Teo <bradleyteowj@gmail.com>
2021-11-06 16:27:29 -0400

    Add login API

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,34 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_API_URL;
+const axiosInstance = axios.create({
+  validateStatus: (status) => true,
+  headers: {
+    "x-access-token": process.env.REACT_APP_API_TOKEN,
+  },
+});
 
 const LoginButton = (props) => {
+  const [loading, setLoading] = useState(false);
   const history = useHistory();
 
-  const logout = () => {
-    window.localStorage.removeItem("accessToken");
-    props.setState.setLoading(true);
-    props.setState.setLoggedIn(false);
-    props.setState.setLoading(false);
-    // Check if currently on FCE page
-    if (
-      history.location.pathname &&
-      history.location.pathname.includes("/fce")
-    ) {
-      history.push("/course");
+  useEffect(() => {
+    if (window.localStorage.getItem("course_token")) {
+      history.push("/");
     }
+  }, []);
+
+  const popupCenter = (title, w, h) => {
+    const dualScreenLeft =
+      window.screenLeft !== undefined ? window.screenLeft : window.screenX;
+    const dualScreenTop =
+      window.screenTop !== undefined ? window.screenTop : window.screenY;
+
+    const width = window.innerWidth
+      ? window.innerWidth
+      : document.documentElement.clientWidth
+      ? document.documentElement.clientWidth
+      : window.screen.width;
+    const height = window.innerHeight
+      ? window.innerHeight
+      : document.documentElement.clientHeight
+      ? document.documentElement.clientHeight
+      : window.screen.height;
+
+    const systemZoom = width / window.screen.availWidth;
+    const left = (width - w) / 2 / systemZoom + dualScreenLeft;
+    const top = (height - h) / 2 / systemZoom + dualScreenTop;
+    const newWindow = window.open(
+      "about:blank",
+      title,
+      `
+      scrollbars=yes,
+      width=${w / systemZoom}, 
+      height=${h / systemZoom}, 
+      top=${top}, 
+      left=${left}
+      `
+    );
+
+    newWindow?.focus();
+    return newWindow;
   };
 
   return (
     <div>
       {props.state.loggedIn ? (
-        <Button
-          className="float-right"
-          variant="outline-primary"
-          size="sm"
-          onClick={logout}
-        >
+        <Button className="float-right" variant="outline-primary" size="sm">
           Logout
         </Button>
       ) : (
@@ -37,10 +70,46 @@ const LoginButton = (props) => {
           variant="outline-primary"
           size="sm"
           as="a"
-          href={
-            process.env.REACT_APP_LOGIN_API +
-            encodeURIComponent(window.location.href)
-          }
+          onClick={() => {
+            setLoading(true);
+            const loginWindow = popupCenter("Login with CMU Email", 400, 600);
+            axiosInstance
+              .get(BASE_URL + "/auth/signRequest")
+              .then((response) => {
+                if (response.data.token) {
+                  if (loginWindow) {
+                    loginWindow.location.href =
+                      "https://login.scottylabs.org/login/" +
+                      response.data.token;
+                  } else {
+                    alert("Unable to create login request");
+                  }
+                }
+              });
+            window.addEventListener(
+              "message",
+              (event) => {
+                if (event.origin !== "https://login.scottylabs.org") {
+                  return;
+                } else {
+                  window.localStorage.setItem("course_token", event.data);
+                  axiosInstance
+                    .post(BASE_URL + "/auth/login", {
+                      token: event.data,
+                    })
+                    .then(() => {
+                      history.push("/");
+                      setLoading(false);
+                    })
+                    .catch(() => {
+                      setLoading(false);
+                      alert("Failed to log in");
+                    });
+                }
+              },
+              false
+            );
+          }}
         >
           Log In to Access FCE Data
         </Button>
